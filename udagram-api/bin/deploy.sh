cf8c08a56fb2d12b5be57c53b8b8f800823b9aa2
Shimo Mahmoud <eng.shimaamahmoud@gmail.com>
2022-04-17 00:09:53 +0200

    update documentation

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
-eb setenv PORT=$PORT USERNAME=$USERNAME
+eb setenv PORT=4200 USERNAME=Shimaa
 eb init udagram-ebservice --platform Node.js --region 'us-east-1' 
-eb deploy Udagramebservice-env
+eb deploy udagram-ebservice

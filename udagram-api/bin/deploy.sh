2fd367414e8a72513847e724070f6849faacdb53
Shimo Mahmoud <eng.shimaamahmoud@gmail.com>
2022-04-17 02:12:37 +0200

    update env

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,5 @@
+eb setenv PORT=3000 USERNAME=aws-cli
+sleep 5s
 eb init udagram-ebservice --platform Node.js --region 'us-east-1' 
 eb deploy udagram-ebservice
-sleep 5s
-eb setenv PORT=3000 USERNAME=aws-cli
+

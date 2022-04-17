d66ade2d3cc392701c95ff20690ab8b2588b3d73
Shimo Mahmoud <eng.shimaamahmoud@gmail.com>
2022-04-17 02:04:46 +0200

    add wait

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
-$ eb setenv PORT=3000 USERNAME=aws-cli
 eb init udagram-ebservice --platform Node.js --region 'us-east-1' 
-
 eb deploy udagram-ebservice
+sleep 5s
+eb setenv PORT=3000 USERNAME=aws-cli

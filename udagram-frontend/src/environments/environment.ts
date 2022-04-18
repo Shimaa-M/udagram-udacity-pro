0e3866eec7ee7695084567f6b64a1f0a87e75b8c
Shimo Mahmoud <eng.shimaamahmoud@gmail.com>
2022-04-18 21:12:04 +0200

    changes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagramebservice-env.eba-tb3n9xvi.us-east-1.elasticbeanstalk.com/'
+  apiHost: 'http://udagramebservice-env.eba-tb3n9xvi.us-east-1.elasticbeanstalk.com/api/v0/'
 };
 
 /*

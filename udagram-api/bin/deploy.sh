b1d46651ed85e74d9eb03193621b33baa08010e8
Shimo Mahmoud <eng.shimaamahmoud@gmail.com>
2022-04-18 22:27:00 +0200

    update env variables

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 eb init udagram-ebservice --platform Node.js --region 'us-east-1'  
-eb setenv PORT=3000 USERNAME=aws-cli 
+eb setenv PORT=3000 USERNAME=aws-cli URL=postgres://postgres:Saeed7abiby@database-1.cfxas0fk51uq.us-east-1.rds.amazonaws.com:5432/udagram_db AWS_REGION=us-east-1a JWT_SECRET=i-love-programming
 eb deploy Udagramebservice-env
 
 
